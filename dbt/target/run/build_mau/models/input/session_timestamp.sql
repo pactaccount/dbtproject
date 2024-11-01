79e7ca9cd3a149311cd28eaddd6d0383f8fdd157
Abhinav Vummidichetty <152488502+pactaccount@users.noreply.github.com>
2024-11-01 12:38:07 -0700

    Add files via upload

---
+++ dbt/target/run/build_mau/models/input/session_timestamp.sql
@@ -0,0 +1,10 @@
+
+  create or replace   view dev.analytics.session_timestamp
+  
+   as (
+    SELECT
+    sessionId,
+    ts
+FROM dev.raw_data.session_timestamp
+  );
+
---
+++ dbt/target/run/build_mau/models/input/user_session_channel.sql
@@ -0,0 +1,11 @@
+
+  create or replace   view dev.analytics.user_session_channel
+  
+   as (
+    SELECT
+    userId,
+    sessionId,
+    channel
+FROM dev.raw_data.user_session_channel
+  );
+
